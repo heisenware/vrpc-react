23d96215e78b36516658dc6413e0455f4b3141bb
Burkhard Heisen <burkhard.heisen@xsmail.com>
2021-02-03 15:35:35 +0100

    corrected path

--- examples/install-prod.sh
+++ examples/install-prod.sh
@@ -7,14 +7,14 @@ cd $(dirname `[[ $0 = /* ]] && echo "$0" || echo "$PWD/${0#./}"`)
 
 DIR=$PWD
 
-cd ../examples/vrpc-react-todos-1/frontend
+cd vrpc-react-todos-1/frontend
 
 npm install --only=prod
 npm run build
 
 cd $DIR
 
-cd ../examples/vrpc-react-todos-2/frontend
+cd vrpc-react-todos-2/frontend
 
 npm install --only=prod
 npm run build
